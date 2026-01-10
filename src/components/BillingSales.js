4d137180ed8a5bf2deb329a6e43b4e1aa4b166d2
inaradesignsindia <inaradesignsindia@gmail.com>
2026-01-10 06:32:30 +0530

    Add CSS import to BillingSales component

--- src/components/BillingSales.js
+++ src/components/BillingSales.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import '../styles/BillingSales.css';
 
 function BillingSales({ inventory, onAddBill }) {
   const [selectedProducts, setSelectedProducts] = useState([]);
